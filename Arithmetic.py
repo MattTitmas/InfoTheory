084a8a431c5f9b9a241b86bc5c770be4db636f6d
MattTitmas <mct52@cam.ac.uk>
2022-11-20 15:43:08 +0000

    feat: add argparse for Arithmetic.py

---
+++ Arithmetic.py
@@ -0,0 +1,17 @@
+import argparse
+
+def main(alphabet: List[str], probability: List[float], to_encode: str):
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Retrieve the Arithmetic code for a set of symbols.')
+    parser.add_argument('-a', '--alphabet', type=str, nargs='+', required=True,
+                        help='List of symbols in the alphabet.')
+    parser.add_argument('-p', '--probability', type=float, nargs='+', required=True,
+                        help='Probabilities of given symbols.')
+    parser.add_argument('-s', '--string', type=str, required=True,
+                        help='String to encode.')
+    args = parser.parse_args()
+
+    main(args.alphabet, args.probability, args.string)
