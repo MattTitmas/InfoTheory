460d8e83fa3d10fe1986e23b24ec8ca5b369a8ac
MattTitmas <mct52@cam.ac.uk>
2023-05-24 11:27:39 +0100

    feat: add verbosity to Arithmetic.py

--- Arithmetic.py
+++ Arithmetic.py
@@ -2,26 +2,35 @@ import argparse
 from typing import List, Tuple
 
 
-def arithmetic_encoding(alphabet: List[Tuple[str, float]], to_encode: str) -> Tuple[float, float]:
+def arithmetic_encoding(alphabet: List[Tuple[str, float]], to_encode: str,
+                        verbose: bool = False) -> Tuple[float, float]:
     alphabet = sorted(alphabet, key=lambda x: x[1], reverse=True)
 
     lower_limit = 0
     upper_limit = 1
 
-    for char in to_encode:
+    for count, char in enumerate(to_encode, start=1):
         limit_range = upper_limit - lower_limit
-
+        if verbose:
+            print(f'Iteration {count}:\n'
+                  f'\tRange: [{lower_limit}, {upper_limit}]\n'
+                  f'\tNext char: {char}')
         char_index = [symbol[0] for symbol in alphabet].index(char)
         prob_below = sum([alphabet[i][1] for i in range(char_index)])
         prob_above = 1 - (prob_below + alphabet[char_index][1])
+        if verbose:
+            print(f'\tProbBelow, ProbAbove, LimitRange: [{prob_below}, {prob_above}, {limit_range}]')
+            print(f'\tLowerLimit, UpperLimit: [LowerLimit + (LimitRange * ProbBelow), UpperLimit - (LimitRange * '
+                  f'ProbAbove)]')
         lower_limit += (limit_range * prob_below)
         upper_limit -= (limit_range * prob_above)
 
     return lower_limit, upper_limit
 
 
-def main(alphabet: List[str], probabilities: List[float], to_encode: str):
-    lower_limit, upper_limit = arithmetic_encoding(list(zip(alphabet, probabilities)), to_encode)
+def main(alphabet: List[str], probabilities: List[float], to_encode: str,
+         verbose: bool = False):
+    lower_limit, upper_limit = arithmetic_encoding(list(zip(alphabet, probabilities)), to_encode, verbose)
     print(f'The range of the encoded symbol is {lower_limit}, {upper_limit}.')
 
 
@@ -31,8 +40,10 @@ if __name__ == '__main__':
                         help='List of symbols in the alphabet.')
     parser.add_argument('-p', '--probability', type=float, nargs='+', required=True,
                         help='Probabilities of given symbols.')
-    parser.add_argument('-s', '--string', type=str, required=True,
+    parser.add_argument('-e', '--encode', type=str, required=True,
                         help='String to encode.')
+    parser.add_argument('-v', '--verbose', action='store_true',
+                        help='Verbose output.')
     args = parser.parse_args()
 
-    main(args.alphabet, args.probability, args.string)
+    main(args.alphabet, args.probability, args.encode, args.verbose)
